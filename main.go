a47c149d3ef514f1f84d2d4c6a231ccc97dda59f
thatNitinVinayak <thatnitinvinayak@icloud.com>
2023-08-03 12:17:50 +0530

    modified: main.go

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 import (
 	"fmt"
-	"time"
-	"GOlang-Training/calculator"
 )
 
 func main() {
-	go append()
-	sum()
+	ch := make(chan int)
+	go goRoutine(ch)
+	ch <- 10
 }
 
-func append() {
-	for i := 0; i < 100; i++ {
-		calculator.InsertNumber(i)
-		fmt.Println("Inserted : ", i)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func sum() {
-	for i := 0; i < 100; i++ {
-		sum := calculator.GetSum()
-		fmt.Println("Sum : ", sum)
-		time.Sleep(5 * time.Second)
-	}
+func goRoutine(ch chan int) {
+	fmt.Println("Channel Value:", <-ch)
 }
