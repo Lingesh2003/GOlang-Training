3d393520e524ce0e5caa32688a184de992449d95
thatNitinVinayak <thatnitinvinayak@icloud.com>
2023-08-04 11:02:58 +0530

    added: fileWriting2.go

---
+++ fileWriting2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func fileWriting2() {
+	file, _ := os.OpenFile("fileWriting2.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	defer file.Close()
+
+	var name, notes string
+	fmt.Print("Name : ")
+	fmt.Scanln(&name)
+	fmt.Print("Notes :")
+	fmt.Scanln(&notes)
+
+	scn := bufio.NewScanner(file)
+
+	for scn.Scan() {
+		time.Sleep(500 * time.Millisecond)
+		line := scn.Text()
+		fmt.Println(line)
+	}
+
+	fmt.Fprintln(file, name, ",", notes)
+}
