815ff2c282d0785e848b2b2b9552fe6c297d7df6
thatNitinVinayak <thatnitinvinayak@icloud.com>
2023-08-04 11:43:43 +0530

    fileWriting2.go

--- fileWriting2.go
+++ fileWriting2.go
@@ -11,11 +11,11 @@ func fileWriting2() {
 	file, _ := os.OpenFile("fileWriting2.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 	defer file.Close()
 
-	var name, notes string
+	var name, msg string
 	fmt.Print("Name : ")
 	fmt.Scanln(&name)
-	fmt.Print("Notes :")
-	fmt.Scanln(&notes)
+	fmt.Print("Message :")
+	fmt.Scanln(&msg)
 
 	scn := bufio.NewScanner(file)
 
@@ -25,5 +25,5 @@ func fileWriting2() {
 		fmt.Println(line)
 	}
 
-	fmt.Fprintln(file, name, ",", notes)
+	fmt.Fprintln(file, name, ",", msg)
 }
