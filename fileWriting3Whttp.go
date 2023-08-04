3ed9750eeea4def10c0b2e32953006843c2b3073
thatNitinVinayak <thatnitinvinayak@icloud.com>
2023-08-04 16:41:55 +0530

    fileWriting3Whttp.go

---
+++ fileWriting3Whttp.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type Notezz struct {
+	user string
+	msg  string
+}
+
+var prsn = map[string][]string{}
+
+func fileWriting3Whttp() {
+	N := []Notezz{}
+	Notezz := Notezz{}
+	b, _ := os.OpenFile("fileWriting3.txt", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+
+	defer b.Close()
+
+	// fmt.Fprintln(b, "Nitin, Konnichiwa")
+
+	scan := bufio.NewScanner(b)
+	for scan.Scan() {
+		time.Sleep(500 * time.Millisecond)
+		line := scan.Text()
+		str := strings.Split(line, ",")
+		prsn[str[0]] = append(prsn[str[0]], str[1])
+		Notezz.user = str[0]
+		Notezz.msg = str[1]
+
+		N = append(N, Notezz)
+	}
+	fmt.Println(prsn)
+	
+	http.HandleFunc("/", HTTPhandler)
+	port := "8080"
+	fmt.Printf("Server Started on https://localhost:%s \n", port)
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		fmt.Println("Error Starting trhe Server: ", err)
+	}
+}
+
+func HTTPhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, prsn)
+}
